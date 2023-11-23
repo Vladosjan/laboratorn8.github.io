87a7f688f5aa2d948e019983d240dddc6babe63b
Vladosjan <150368423+Vladosjan@users.noreply.github.com>
2023-11-23 12:43:09 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,6 @@ let body = document.querySelector("body");
  body.style.width = "100%"; 
  body.style.height = "25%"; 
  body.style.backgroundColor = "blue";  
-
-
   let firstLabel = document.getElementById("label1").innerHTML =  prompt("Enter your name:");  
   let firstLabel1 = document.getElementById("label2");
   let textInput1 = document.getElementById("input1");  
@@ -14,11 +12,9 @@ let body = document.querySelector("body");
    let test = document.getElementById("button1"); 
  for(let i = 0; i < 3; i++){   
 test.onclick = Game; 
-
 let random = Math.floor(Math.random() * 2);  
 let random1 = Math.floor(Math.random() * 2);   
-   
- let circle1 = document.getElementById("circle1");   
+  let circle1 = document.getElementById("circle1");   
 circle1.style.fontFamily = "Times New Roman" 
  circle1.style.fontSize = "30px"; 
   circle1.style.fontWeight = "bold";
@@ -26,26 +22,16 @@ circle1.style.fontFamily = "Times New Roman"
 circle2.style.fontFamily = "Times New Roman" 
  circle2.style.fontSize = "30px"; 
   circle2.style.fontWeight = "bold";       
-
-  
- function Game(){  
-   
-   if(random == 1){  
-    
-    circle1.innerHTML = random;
+   function Game(){  
+      if(random == 1){  
+        circle1.innerHTML = random;
      circle1.innerHTML = "You are win	" + random; 
-	
-
-   }if(random1 == 1){ 
+	   }if(random1 == 1){ 
       circle2.innerText = random;
       circle2.innerHTML = "You are lose	" + random;
-    
-   }
-   
-   }
+       }
+      }
   }
-   
-
 }
 );
 
