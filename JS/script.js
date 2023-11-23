8f96b8116b9237340e9b172a58dee75d35b181c3
Vladosjan <150368423+Vladosjan@users.noreply.github.com>
2023-11-23 12:55:46 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -27,7 +27,7 @@ circle2.style.fontFamily = "Times New Roman"
       if(random == 1){  
         circle1.innerHTML = random;
      circle1.innerHTML = "You are win	" + random; 
-	   }else{ 
+	   }if(random1 == 1){ 
       circle2.innerText = random;
       circle2.innerHTML = "You are lose	" + random;
        }
